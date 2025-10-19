367c605baa8d98d2d47fe82fc270832334dde818
lazerdsgn <lazerdsgnworks@gmail.com>
2025-10-20 04:21:08 +0500

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'lazerdsgnworks-crypto/lazerdsgn1'
+  base: '/lazerdsgn1/'
 });
