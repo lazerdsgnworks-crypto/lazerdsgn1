49a5c60c9caffde760cba08884920460d1442114
lazerdsgn <lazerdsgnworks@gmail.com>
2025-11-06 07:32:52 +0000

    Updated config and deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lazerdsgn1/', // 👈 base path for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
-      // FIX: `__dirname` is not available in ES modules by default.
-      // Replaced it with a path resolution from the current working directory,
-      // which is the project root when running Vite.
-      "@": path.resolve("./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
